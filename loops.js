492a759ee54d799ce0051eb6ba0ce0f771aead52
Vik Vangala <vvang044@gmail.com>
2019-05-29 17:37:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,5 +19,13 @@ function whileLoop(n) {
   }
   
 function doWhileLoop(num){
-  
+  function incrementVariable(){
+    i = i + 1;
+    return i
+  }
+   
+   do {
+     console.log("I run once regardless.");
+   }
+    while (incrementVariable() < num);
 }
